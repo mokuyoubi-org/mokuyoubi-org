5b12ece0d56d5761363f9184024e88922488d928
Mokuyoubi <mokuyoubi@Harutakas-MacBook-Air.local>
2026-08-19 08:28:27 +0900

    update

--- posts/2026-08-18.js
+++ posts/2026-08-18.js
@@ -1,7 +1,7 @@
 window.currentPost = {
-  title: "タブ",
+  title: "最近",
   content: `
-・Expoをアップデートしたら、Androidで、タブを押すと勝手に水紋みたいなエフェクトがつくのが嫌で、それを避けるために独自のタブボタンにしたら、
+🌟Expoをアップデートしたら、Androidで、タブを押すと勝手に水紋みたいなエフェクトがつくのが嫌で、それを避けるために独自のタブボタンにしたら、
 今度はwebで開くと、タブを切り替えるたびにリロードが起こる、というおかしな状況になっていたが、それを直すことに成功：
 
       ＜Tabs
@@ -18,5 +18,25 @@ window.currentPost = {
                   return <Pressable {...restProps} android_ripple={null} />;
                 },
 （以下略）
+
+簡単な説明。
+
+・そもそも、<a>タグがクリックされた時、普通はリロード(くるくる)するのが普通。JS製のサイトなど。
+・でも、Expoのrouterは特殊な働きで、<a>タグのクリックを横取りして(PlatformPressableというコンポーネントが担当)、リロードの代わりにURLだけこっそり書き換えてくれてた
+・でも、Androidのripple effectが嫌だったので、そのPlatformPressableをやめて自前のボタン(素のPressable)に差し替えた
+・そしたらrippleは消えたが、クリックを横取りする係もいなくなり、<a>タグ本来の動作(問答無用でページ丸ごと再取得)が素通りするようになって、毎回リロードするようになった
+・戻せばrippleが復活する
+・そこで、両方を解決させる案として、処理を端末で分岐させた(Webは標準のPlatformPressableのまま、ネイティブだけ自前のPressableに差し替え)
+
+🌟deprecatedな書き方。
+// 修正前
+<View pointerEvents="box-none">
+// 修正後
+<View style={{ pointerEvents: "box-none" }}>
+
+// 修正前
+noShadow: { shadowOpacity: 0,},
+// 修正後
+noShadow: { boxShadow: "none",},
   `,
 };
