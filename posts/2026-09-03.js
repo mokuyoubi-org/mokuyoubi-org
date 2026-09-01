96b6bc7aea587683f9b683a078497f8ab3ff3f1c
Mokuyoubi <mokuyoubi@Harutakas-MacBook-Air.local>
2026-09-02 02:04:18 +0900

    update

--- posts/2026-09-03.js
+++ posts/2026-09-03.js
@@ -1,22 +1,22 @@
 window.currentPost = {
   title: "Expoのweb版をボットから守る",
   content: `
-expoアプリをwebで公開したら、ボットから守りましょう
+expoアプリをwebで公開したら、ボットから守ろう
+
 特に僕のアプリのように、自動でユーザを生成するようなアプリでは必須。
-バックエンドはsupabase、アプリのwebビルドとホストはcloudflare、という黄金コンビとします。
+バックエンドはsupabase、アプリのwebビルドとホストはcloudflare、というコンビとする。
 
 ①cloudflareの左上の検索窓でTurnstileと調べる。
 
-②setup with spinというのを選択する。
+setup with spinというのを選択する。
 あとは、アプリを選択したら、site key と secretが表示されたと思う。あとはAIに対するプロンプト。
 プロンプトはAIに貼って教えてもらってください。site keyとsecretはどこかにメモしておいてください
 
-③supabaseに行って、authenticationから、attack protectionを選択。
+②supabaseに行って、authenticationから、attack protectionを選択。
 Enable Captcha protectionというのがあるのでそのトグルをオンにし、
 Choose Captcha Providerはturnstileを選択、Captcha secretはさっきメモしたsecretをセットして、保存する。
 
-
-④TurnstileWidgetをnative/web両方用作って、認証系、ログイン系のコードに埋め込む
+③Expoで、TurnstileWidgetをnative/web両方用作って、認証系、ログイン系のコードに埋め込む
 詳しくはclaudeくんに聞いてください
   `,
 };
